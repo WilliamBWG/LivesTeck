7e3dbb76491d644a4cce256069c1b94b9b777e44
WilliamBWG <william.b26@aluno.ifsc.du.br>
2025-05-07 11:28:07 -0300

    Tela Granja

--- app/src/main/java/br/com/bwg/livesteck/ui/granja/CadGranjaFragment.java
+++ app/src/main/java/br/com/bwg/livesteck/ui/granja/CadGranjaFragment.java
@@ -1,5 +1,6 @@
 package br.com.bwg.livesteck.ui.granja;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,22 +8,45 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import br.com.bwg.livesteck.R;
+import br.com.bwg.livesteck.model.Granja;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CadGranjaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CadGranjaFragment extends Fragment {
+public class CadGranjaFragment extends Fragment implements Response.ErrorListener, Response.Listener, View.OnClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private EditText etNome;
+    private EditText etLat;
+    private EditText etLong;
+    private EditText etProprietario;
+    private EditText etSilos;
+    private Button btSalvar;
     private View view;
+    //volley
+    private RequestQueue requestQueue;
+    private JsonObjectRequest jsonObjectReq;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,6 +88,85 @@ public class CadGranjaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         this.view = inflater.inflate(R.layout.fragment_cad_granja, container, false);
+        //
+        //Binding dos Objetos com os componentes XML
+        this.etNome = (EditText) view.findViewById(R.id.etNome);
+        this.etLat = (EditText) view.findViewById(R.id.etLat);
+        this.etLong = (EditText) view.findViewById(R.id.etLong);
+        this.etProprietario = (EditText) view.findViewById(R.id.etProprietario);
+        this.etSilos = (EditText) view.findViewById(R.id.etSilos);
+        this.btSalvar = (Button) view.findViewById(R.id.btSalvar);
+        //definindo o listener do botão
+        this.btSalvar.setOnClickListener(this);
+        //instanciando a fila de req
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+        //inicializando a fila
+        this.requestQueue.start();
+        //
         return this.view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //verificando se é o botão salvar
+        if (view.getId() == R.id.btSalvar) {
+            //instanciando objeto de negócio
+            Granja g = new Granja();
+            //populando objeto com dados da tela
+            g.setNome(this.etNome.getText().toString());
+            g.setLatitude(this.etLat.getText().toString());
+            g.setLongitude(this.etLong.getText().toString());
+            g.setProprietario(this.etProprietario.getText().toString());
+            g.setSilos(Integer.parseInt(this.etSilos.getText().toString()));
+            //
+            jsonObjectReq = new JsonObjectRequest(
+                    Request.Method.POST,
+                    "http://10.0.2.2:8080/seg/cadusuario.php",
+                    g.toJsonObject(), this, this);
+            requestQueue.add(jsonObjectReq);
+
+            //mensagem de sucesso
+            Context context = view.getContext();
+            CharSequence text = "salvo com sucesso!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Snackbar mensagem = Snackbar.make(this.view,
+                "Ops! Houve um problema ao realizar o cadastro: " +
+                        error.toString(),Snackbar.LENGTH_LONG);
+        mensagem.show();
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        try {
+//instanciando objeto para manejar o JSON que recebemos
+            JSONObject jason = new JSONObject(response.toString());
+//context e text são para a mensagem na tela o Toast
+            Context context = view.getContext();
+//pegando mensagem que veio do json
+            CharSequence mensagem = jason.getString("message");
+//duração da mensagem na tela
+            int duration = Toast.LENGTH_SHORT;
+//verificando se salvou sem erro para limpar campos da tela
+            if (jason.getBoolean("success")){
+//limpar campos da tela
+                this.etNome.setText("");
+                this.etLat.setText("");
+                this.etLong.setText("");
+                this.etProprietario.setText("");
+                this.etSilos.setText("0");
+            }
+//mostrando a mensagem que veio do JSON
+            Toast toast = Toast.makeText (context, mensagem, duration);
+            toast.show();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
